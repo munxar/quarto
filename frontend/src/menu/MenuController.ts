81d90528dd90d8efc5c5eeb39b3a3e66686dc1dc
saschaaeppli <sascha.aeppli@greenmatch.ch>
2015-11-18 17:40:25 +0100

    added logout menu.

--- frontend/src/menu/MenuController.ts
+++ frontend/src/menu/MenuController.ts
@@ -5,25 +5,17 @@ import {RouteConfiguration} from "../interfaces";
 import {TokenService} from "../TokenService";
 
 export class MenuController {
-    items:MenuItem[];
 
     constructor(route:RouteConfiguration, private tokenService: TokenService) {
-        var visiblePublic = () => !tokenService.getToken();
-        var visiblePrivate = () => tokenService.getToken();
 
-        this.items = [
-            //new MenuItem(route.home, "home", visiblePrivate),
-            //new MenuItem("/chat", "chat", visiblePrivate),
-            //new MenuItem(route.about, "about", visiblePrivate),
-            //new MenuItem(route.login, "login", visiblePublic),
-            //new MenuItem("/signup", "signup", visiblePublic),
-            new MenuItem("/logout", "logout", visiblePrivate)
-        ];
     }
 
-    routeTo(item: MenuItem) {
-        m.route(item.href);
+    isLoggedIn() {
+        return this.tokenService.getToken() != null;
     }
 
-
+    logout = () => {
+        this.tokenService.removeToken();
+        m.route("/");
+    };
 }
--- frontend/src/menu/menuView.ts
+++ frontend/src/menu/menuView.ts
@@ -4,8 +4,9 @@ import {MenuController} from "./MenuController";
 import {MenuItem} from "./MenuItem";
 
 export function menuView(ctrl: MenuController) {
+
     return [
         m(".logo.menu-item", "chat!"),
-        m(".account.menu-item", "account")
+        m(".account.menu-item", ctrl.isLoggedIn() ? m("div", { onclick: ctrl.logout }, "logout") : m("div"))
     ];
 }
